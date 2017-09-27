0faabc8b3688acad17d00dd638025b215fb5d1b8
imgss <838632526@qq.com>
2017-09-27 21:59:54 +0800

    支持选择框

--- table.js
+++ table.js
@@ -1,46 +1,36 @@
 (function($){
     var data = {
-        activeCell:null
+        activeCell: null,
+        selects: {},
+        selectCols: []
     };
-    function findCell(cell, direction){
-        var indexof = [].indexOf, 
-            line = $(cell).parent()[0],
-            index = indexof.call(line.children, cell);
-        switch (direction){
-        case 'DOWN':
-            var nextLine = line.nextElementSibling;
-            if(nextLine){
-                return $(nextLine).children()[index];
-            }else{
-                return null;
-            }
-        case 'UP':
-            var prevLine = line.previousElementSibling;
-            if(prevLine){
-                return $(prevLine).children()[index];
-            }else{
-                return null;
-            }
-        case 'LEFT':
-            return cell.previousElementSibling;
-        case 'RIGHT':
-            return cell.nextElementSibling;
-        }
-
-    }
     $.fn.table = function(options){
         options = $.extend(options, {
-            inputCss: {padding: '0.75em'}
+            inputCss: {padding: '0.75em'},
+            cellType: 'td',
+            select:{
+                3:['A','B','c','D']
+            }
         });
         var wrapper = this.wrap('<div id="table-editable"></div>').parent();
         wrapper.append('<input \
                     type="text" \
                     style="box-sizing: border-box;display:none;position:absolute; \
                     ">');
+        var selectCols = data.selectCols = Object.keys(options.select);//由显示选择框的列号构成的数组
+        if(selectCols.length  > 0){
+            for(var i = 0, len = selectCols.length; i < len; i++){
+                var colNum = selectCols[i];
+                var selectOptions = options.select[colNum];
+                var select = data.selects[colNum] = createSelect(selectOptions);
+                wrapper.append(select);
+            }
+        }
         var input = wrapper.find('input');
         input.css(options.inputCss);
         wrapper.on('click', 'td', function(event){
             var cell = event.target;
+            var index = getIndexOf(cell);
             if(data.activeCell){//在移动input之前更新cell的值为input的值
                 var inputValue = input.val();
                 data.activeCell.innerHTML = inputValue;
@@ -50,6 +40,11 @@
             var left = cell.offsetLeft;
             var top = cell.offsetTop;
             var value = cell.innerHTML;
+            if(data.selectCols.indexOf(String(index)) !== -1){
+                input.hide();
+                input = $(data.selects[index]);
+                input.show();
+            }
             if(value){//将值填写到input中
                 
                 input.val(value);
@@ -97,4 +92,44 @@
 
         });
     };
+    function findCell(cell, direction){
+        var indexof = [].indexOf, 
+            line = $(cell).parent()[0],
+            index = indexof.call(line.children, cell);
+        switch (direction){
+        case 'DOWN':
+            var nextLine = line.nextElementSibling;
+            if(nextLine){
+                return $(nextLine).children()[index];
+            }else{
+                return null;
+            }
+        case 'UP':
+            var prevLine = line.previousElementSibling;
+            if(prevLine){
+                return $(prevLine).children()[index];
+            }else{
+                return null;
+            }
+        case 'LEFT':
+            return cell.previousElementSibling;
+        case 'RIGHT':
+            return cell.nextElementSibling;
+        }
+
+    }
+    function createSelect(options){
+        if (Object.prototype.toString.call(options) !== '[object Array]'){
+            throw new Error('选项应该是数组');
+        }
+        var htmlStr = '<select style="display:none;position:absolute">';
+        for(var i = 0,len = options.length; i < len; i++){
+            htmlStr += '<option>' + options[i] + '</option>';
+        }
+        htmlStr += '</select>';
+        return $(htmlStr)[0];
+    }
+    function getIndexOf(cell){
+        return $(cell).parent().children().index(cell);
+    }
 })(jQuery);
